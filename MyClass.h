5f919c42827b28514eca51fa55229cb77b5abc4c
Julia Humelanc <humelanz@gmail.com>
2019-03-03 21:27:24 +0100

    zmiana kodu z uwzględnieniem klasy

--- MyClass.h
+++ MyClass.h
@@ -8,6 +8,19 @@
 #include<iomanip>
 #include<cmath>
 
+class Function
+{   public:
+    double a,b,c;
+    void readF() {
+        std::cout << "Podaj parametry a,b,c funkcji";
+        std::cout << "\na=";
+        std::cin >> a;
+        std::cout << "\nb=";
+        std::cin >> b;
+        std::cout << "\nc=";
+        std::cin >> c;
+    }
+};
 
 double delta(double i, double j, double k);
 void zero(double a, double b, double c);
--- zeromain.cpp
+++ zeromain.cpp
@@ -1,15 +1,12 @@
 #include "MyClass.h"
 
 int main(){
-    double a,b,c;
-    std::cout <<"Podaj parametry a,b,c funkcji";
-    std::cout <<"\na=";
-    std::cin >> a;
-    std::cout <<"\nb=";
-    std::cin >> b;
-    std::cout <<"\nc=";
-    std::cin >> c;
-    zero(a,b,c);
+
+    Function f;
+
+    f.readF();
+
+    zero(f.a,f.b,f.c);
 
     return 0;
 }
